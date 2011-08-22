7bb1d58a1ff5b01f5509b13e294ca4c5674bde13
Chris McCord <chris@chrismccord.com>
2011-08-22 08:37:19 -0400

    Improve backwards (and forwards) animations

--- app/assets/coffeescripts/coffeebook.coffee
+++ app/assets/coffeescripts/coffeebook.coffee
@@ -34,25 +34,100 @@ class @BookView extends Backbone.View
   defaults:
     width: 940
     height: 800
-
+    speedFactor: 0.30
   pageNumber: 0
+  activePageView: null
+  nextPageView: null
   pages: new Pages([
-      new Page({ title: 'page 1', content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}),
-      new Page({ title: 'page 2', content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."}),
+      new Page({ title: 'page 1', content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"})
+      new Page({ title: 'page 2', content: "jskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs f<img src='images/picture1.jpg'/>"})
+      new Page({ title: 'page 3', content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.<img src='images/picture2.jpg'/>"})
+
     ])
 
   init: ->
     @goTo(0)
+    
+  animateForward: (callback) ->
+    # @defaults.speedFactor = 1
+    width = $(".page.active").width()
+    $turn = $("#turn")
+    $turn_middle = $("#turn .middle")
+    $active = $(".page.active").parent()
+ 
+    $turn.css({"right": '0px'})
+    $turn_middle.css({"width": '34px'})
+    $turn.stop().show().animate {right: width + 20}, 1800 * @defaults.speedFactor,'linear', ->
+      $turn.fadeOut 50, ->
+        callback() if callback
+
+    $turn_middle.animate({width: width * 1.25}, 1800 * @defaults.speedFactor,'linear')
+
+    $active.animate {width: 0}, 1200 * @defaults.speedFactor, 'linear', ->
+      $active.hide()
+    
+  animateBackward: (callback) ->
+    # @defaults.speedFactor = 6
+    $turn = $("#turn")
+    $next = $(".page.next").parent()
+    $turn_middle = $("#turn .middle")
+    $turn_middle.stop().queue(=>  
+      width = $(".page.active").width()
+      $next.css({"z-index": 2}).hide()
+      $turn.css({"right": '763px'})
+      $turn_middle.css({"width": '928px'})
+      $next.delay(0.35 * 1800 * @defaults.speedFactor)
+        .animate({width: 'toggle'}, 1200 * @defaults.speedFactor, 'linear')      
+    ).dequeue().animate({width: 34}, 1800 * @defaults.speedFactor,'linear')
+    
+    $turn.stop().show().animate {right: 0}, 1800 * @defaults.speedFactor,'linear', ->
+      $turn.fadeOut 50, ->
+        callback() if callback
+
 
 
   # Seek to page model
   #
-  goTo: (pageNumber) ->
+  goTo: (toPageNumber) ->
     #TODO validate this
-    @pageNumber = pageNumber
-    activePageView  = new BookView({el: '#active_page', model: @currentPage() })
-    nextPageView = new BookView({el: '#next_page', model: @nextPage() })
-
+    if toPageNumber > @pageNumber
+      @nextPageView = new BookView({ model: @pages.at(toPageNumber) })
+      $(".page.next").html($(@nextPageView.el).html())
+      @animateForward =>
+        # $("#turn").remove()
+        $(".page.active").parent().remove()
+        $(".page.next").removeClass("next").addClass("active")
+        $("#book").prepend('
+            <div class="right_page_container">
+              <div class="page next">
+              </div>
+            </div>       
+        ')
+      
+    else if toPageNumber < @pageNumber
+      @nextPageView = new BookView({ model: @pages.at(toPageNumber) })
+      $(".page.next").html($(@nextPageView.el).html())
+      @animateBackward =>
+        $("#turn").remove()
+        $(".page.active").parent().remove()
+        $(".page.next").removeClass("next").addClass("active")
+        $("#book").prepend('
+            <div id="turn">
+              <div class="middle">
+              </div>
+              <div class="shadow_left">
+              </div>
+            </div>
+            <div class="right_page_container">
+              <div class="page next">
+              </div>
+            </div>       
+        ')
+    else
+      @activePageView  = new BookView({ model: @currentPage() })
+      $(".page.active").html($(@activePageView.el).html())
+    
+    @pageNumber = toPageNumber
 
   currentPage: -> @pages.at(@pageNumber) 
   nextPage:    -> @pages.at(@pageNumber + 1)
@@ -62,12 +137,13 @@ class @BookView extends Backbone.View
 
 @$(document).ready ->
   CoffeeBook.init()
-  speedFactor = 0.50
-  setTimeout((->
-      $("#turn").animate {right: $("#active_page").width() + 20}, 1800 * speedFactor,'linear', ->
-        $(@).fadeOut(50)
-      $("#turn .middle").animate({width: $("#active_page").width() * 1.25}, 1800 * speedFactor,'linear')
-      
-      $("#active_page").parent().animate {width: 0}, 1200 * speedFactor, 'linear', ->
-            $("#active_page").parent().hide()
-    ), 1000)
+  # setTimeout((->
+  #     speedFactor = CoffeeBook.defaults.speedFactor
+  #     width = $(".page .active").width()
+  #     $("#turn").animate {right: width + 20}, 1800 * speedFactor,'linear', ->
+  #       $(@).fadeOut(50)
+  #     $("#turn .middle").animate({width: width * 1.25}, 1800 * speedFactor,'linear')
+  #     
+  #     $(".page .active").parent().animate {width: 0}, 1200 * speedFactor, 'linear', ->
+  #           $(".page .active").parent().hide()
+  #   ), 1000)
--- public/javascripts/coffeebook.js
+++ public/javascripts/coffeebook.js
@@ -6,7 +6,7 @@
     child.prototype = new ctor;
     child.__super__ = parent.prototype;
     return child;
-  };
+  }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   this.Page = (function() {
     __extends(Page, Backbone.Model);
     function Page() {
@@ -56,32 +56,135 @@
   this.CoffeeBook = {
     defaults: {
       width: 940,
-      height: 800
+      height: 800,
+      speedFactor: 0.30
     },
     pageNumber: 0,
+    activePageView: null,
+    nextPageView: null,
     pages: new Pages([
       new Page({
         title: 'page 1',
         content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
       }), new Page({
         title: 'page 2',
-        content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        content: "jskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs fjskdfj slfkjslkdfjslkdfjlsjdf sldf slkdjfskldjfs flksjdf skdjfs lkdfjs dflksjdf skdlfjkdjlfj slkdfjslkdjfslkdf slkdjfslkdfjs dlfkjsd flksdjf slkdfj sldkfj sldf sdfkjl sldkfjsldkfjs f<img src='images/picture1.jpg'/>"
+      }), new Page({
+        title: 'page 3',
+        content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.<img src='images/picture2.jpg'/>"
       })
     ]),
     init: function() {
       return this.goTo(0);
     },
-    goTo: function(pageNumber) {
-      var activePageView, nextPageView;
-      this.pageNumber = pageNumber;
-      activePageView = new BookView({
-        el: '#active_page',
-        model: this.currentPage()
+    animateForward: function(callback) {
+      var $active, $turn, $turn_middle, width;
+      width = $(".page.active").width();
+      $turn = $("#turn");
+      $turn_middle = $("#turn .middle");
+      $active = $(".page.active").parent();
+      $turn.css({
+        "right": '0px'
       });
-      return nextPageView = new BookView({
-        el: '#next_page',
-        model: this.nextPage()
+      $turn_middle.css({
+        "width": '34px'
       });
+      $turn.stop().show().animate({
+        right: width + 20
+      }, 1800 * this.defaults.speedFactor, 'linear', function() {
+        return $turn.fadeOut(50, function() {
+          if (callback) {
+            return callback();
+          }
+        });
+      });
+      $turn_middle.animate({
+        width: width * 1.25
+      }, 1800 * this.defaults.speedFactor, 'linear');
+      return $active.animate({
+        width: 0
+      }, 1200 * this.defaults.speedFactor, 'linear', function() {
+        return $active.hide();
+      });
+    },
+    animateBackward: function(callback) {
+      var $next, $turn, $turn_middle;
+      $turn = $("#turn");
+      $next = $(".page.next").parent();
+      $turn_middle = $("#turn .middle");
+      $turn_middle.stop().queue(__bind(function() {
+        var width;
+        width = $(".page.active").width();
+        $next.css({
+          "z-index": 2
+        }).hide();
+        $turn.css({
+          "right": '763px'
+        });
+        $turn_middle.css({
+          "width": '928px'
+        });
+        return $next.delay(0.35 * 1800 * this.defaults.speedFactor).animate({
+          width: 'toggle'
+        }, 1200 * this.defaults.speedFactor, 'linear');
+      }, this)).dequeue().animate({
+        width: 34
+      }, 1800 * this.defaults.speedFactor, 'linear');
+      return $turn.stop().show().animate({
+        right: 0
+      }, 1800 * this.defaults.speedFactor, 'linear', function() {
+        return $turn.fadeOut(50, function() {
+          if (callback) {
+            return callback();
+          }
+        });
+      });
+    },
+    goTo: function(toPageNumber) {
+      if (toPageNumber > this.pageNumber) {
+        this.nextPageView = new BookView({
+          model: this.pages.at(toPageNumber)
+        });
+        $(".page.next").html($(this.nextPageView.el).html());
+        this.animateForward(__bind(function() {
+          $(".page.active").parent().remove();
+          $(".page.next").removeClass("next").addClass("active");
+          return $("#book").prepend('\
+            <div class="right_page_container">\
+              <div class="page next">\
+              </div>\
+            </div>       \
+        ');
+        }, this));
+      } else if (toPageNumber < this.pageNumber) {
+        this.nextPageView = new BookView({
+          model: this.pages.at(toPageNumber)
+        });
+        $(".page.next").html($(this.nextPageView.el).html());
+        this.animateBackward(__bind(function() {
+          $("#turn").remove();
+          $(".page.active").parent().remove();
+          $(".page.next").removeClass("next").addClass("active");
+          return $("#book").prepend('\
+            <div id="turn">\
+              <div class="middle">\
+              </div>\
+              <div class="shadow_left">\
+              </div>\
+            </div>\
+            <div class="right_page_container">\
+              <div class="page next">\
+              </div>\
+            </div>       \
+        ');
+        }, this));
+      } else {
+        this.activePageView = new BookView({
+          model: this.currentPage()
+        });
+        $(".page.active").html($(this.activePageView.el).html());
+      }
+      return this.pageNumber = toPageNumber;
     },
     currentPage: function() {
       return this.pages.at(this.pageNumber);
@@ -100,23 +203,6 @@
     }
   };
   this.$(document).ready(function() {
-    var speedFactor;
-    CoffeeBook.init();
-    speedFactor = 0.50;
-    return setTimeout((function() {
-      $("#turn").animate({
-        right: $("#active_page").width() + 20
-      }, 1800 * speedFactor, 'linear', function() {
-        return $(this).fadeOut(50);
-      });
-      $("#turn .middle").animate({
-        width: $("#active_page").width() * 1.25
-      }, 1800 * speedFactor, 'linear');
-      return $("#active_page").parent().animate({
-        width: 0
-      }, 1200 * speedFactor, 'linear', function() {
-        return $("#active_page").parent().hide();
-      });
-    }), 1000);
+    return CoffeeBook.init();
   });
 }).call(this);
