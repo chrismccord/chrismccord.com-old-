aaed351cf679f82d7ae3500997e8af71baf07de0
Chris McCord <chris@chrismccord.com>
2011-11-09 22:00:20 -0500

    Remove twitter tweet fetch

--- server.coffee
+++ server.coffee
@@ -9,17 +9,17 @@ https         = require 'https'
 fs            = require 'fs'
 static        = require 'node-static'
 
-twitter =
-  host: "api.twitter.com"
-  port: 443
-  path: "/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=chris_mccord&count=3"
-
-tweets = []
-https.get(twitter, (res) ->
-  res.on 'data', (data) ->
-    tweets.push(tweet.text) for tweet in JSON.parse(data)
-    console.log sys.inspect tweets
-).on "error", (e) -> console.log "Got error from twitter: " + e.message
+# twitter =
+#   host: "api.twitter.com"
+#   port: 443
+#   path: "/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=chris_mccord&count=3"
+# 
+# tweets = []
+# https.get(twitter, (res) ->
+#   res.on 'data', (data) ->
+#     tweets.push(tweet.text) for tweet in JSON.parse(data)
+#     console.log sys.inspect tweets
+# ).on "error", (e) -> console.log "Got error from twitter: " + e.message
 
 staticServer = new static.Server(Settings.root)
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 (function() {
-  var Settings, fs, http, https, server, static, staticServer, sys, tweets, twitter;
+  var Settings, fs, http, https, server, static, staticServer, sys;
   Settings = {
     root: "./public",
     port: process.env.PORT || 3000
@@ -10,25 +10,6 @@
   https = require('https');
   fs = require('fs');
   static = require('node-static');
-  twitter = {
-    host: "api.twitter.com",
-    port: 443,
-    path: "/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=chris_mccord&count=3"
-  };
-  tweets = [];
-  https.get(twitter, function(res) {
-    return res.on('data', function(data) {
-      var tweet, _i, _len, _ref;
-      _ref = JSON.parse(data);
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        tweet = _ref[_i];
-        tweets.push(tweet.text);
-      }
-      return console.log(sys.inspect(tweets));
-    });
-  }).on("error", function(e) {
-    return console.log("Got error from twitter: " + e.message);
-  });
   staticServer = new static.Server(Settings.root);
   server = http.createServer(function(request, response) {
     return request.addListener('end', function() {
